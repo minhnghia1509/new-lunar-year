1237b2bb433d39b11b3e8b0ba3930f5ea333a9e9
minhnghia1509 <124867257+minhnghia1509@users.noreply.github.com>
2024-02-07 13:26:11 +0700

    Add files via upload
    create

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = "Ms. Hue";
+const giftUrl = "http://nodemy.vn";
+const eventName = "Happy New Lunar Year";
+const titleCard = "For you";
+const contentCard =
+  "Before I met you, I never believed in the harmony between the two souls. In the world of billions and billions of people, how do you know who you want to be with you for the rest of your life? But since you entered my life, suddenly, everything has become perfect. You are the person I have been searching for so long. I hope you will be with me every year. I wish you a good new year, my love!";
+
+const giftImage = "mybae.jpg";
+const base64 = "";
+const giftImageBase64 = "data:image/png;base64, " + base64;
